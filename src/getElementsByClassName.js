c0ea9212e4e5fc528f7fc4692317f6daf152e945
huntertread <huntertread@gmail.com>
2019-10-21 16:29:49 -0700

    pushing up current progress (lots to do still)

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,13 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+var getElementsByClassName = function(className) {
+  // create an output array to push matching nodes up to
+  var outputArr = [];
+  // iterate over the body of the HTML document
+  document.body.each('html', function() {
+    // push any nodes that match "className" to the output array
+  })
+  // return output array
 };
